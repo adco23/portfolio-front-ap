3288985579c465fedf4dba4181db40fd450d3b4f
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-10 22:23:12 -0300

    🎨 env add

--- src/app/services/experiences.service.ts
+++ src/app/services/experiences.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Experience } from '../models/experience';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperiencesService {
 
-  private backendURL: string = 'http://localhost:8080/api/v1/experience';
+  // private backendURL: string = 'http://localhost:8080/api/v1/experience';
+  private backendURL: string = `${environment.apiUrl}/api/v1/experience`;
 
   constructor(
     private httpClient: HttpClient
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
 
-  private backendURL: string = 'http://localhost:8080/api/v1/users/user/1';
+  // private backendURL: string = 'http://localhost:8080/api/v1/users/user/1';
+  private backendURL: string = `${environment.apiUrl}/api/v1/users/user/1`;
 
   constructor(
     private httpClient: HttpClient
