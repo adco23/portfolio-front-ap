659c2f1937d883d4b38f8aedd7a72ffd143a6207
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-27 23:11:08 -0300

    ♻️skill modal config

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Skill } from 'src/app/models/skill';
+import { Skill, skill_types } from 'src/app/models/skill';
 import { SkillsService } from 'src/app/services/skills.service';
 
 @Component({
@@ -16,6 +16,14 @@ export class SkillsComponent implements OnInit {
     isEmpty: true,
   };
   skillForm: FormGroup;
+  modal_config = {
+    show: false,
+    type: '',
+    btnText: '',
+    title: ''
+  };
+  skillTypes: string[] = Object.values(skill_types);
+  submitted: boolean = false;
 
   constructor(
     private skillService: SkillsService,
@@ -42,6 +50,42 @@ export class SkillsComponent implements OnInit {
 
   get form(): any {
     return this.skillForm.controls;
+  };
+
+  openModal(type: string): void {
+    this.modal_config = {
+      type,
+      show: true,
+      title: type === 'add' ? 'Agregar habilidad' : 'Editar habilidad',
+      btnText: type === 'add' ? 'Agregar' : 'Editar',
+    }
+  };
+
+  closeModal(): void {
+    this.modal_config = {
+      show: false,
+      type: '',
+      title: '',
+      btnText: '',
+    }
+  };
+
+  onReset(): void {
+    this.submitted = false;
+    this.skillForm.reset();
+    this.closeModal();
+  };
+
+  onSubmit(): void {
+    if (this.skillForm.invalid) return;
+
+    this.submitted = true;
+
+    this.modal_config.type === 'add' ?
+      console.log('🔥add: ', this.skillForm.value) :
+      console.log('🔥edit: ', this.skillForm.value);
+
+    this.onReset();
   };
 
 }
