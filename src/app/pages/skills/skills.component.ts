3236e8275833caa5e6cbeab4614c26cb07a7e8a4
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-27 21:59:51 -0300

    ✨skill form add

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Skill } from 'src/app/models/skill';
 import { SkillsService } from 'src/app/services/skills.service';
 
@@ -13,14 +14,22 @@ export class SkillsComponent implements OnInit {
   status = {
     isLoaded: false,
     isEmpty: true,
-  }
+  };
+  skillForm: FormGroup;
 
   constructor(
     private skillService: SkillsService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.getSkills();
+
+    this.skillForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      level: null,
+      type: ['', Validators.required]
+    })
   }
 
   private getSkills(): void {
@@ -29,6 +38,10 @@ export class SkillsComponent implements OnInit {
       this.status.isLoaded = true;
       this.status.isEmpty = data.length === 0 ? true : false;
     });
+  };
+
+  get form(): any {
+    return this.skillForm.controls;
   };
 
 }
