238ee2f07312d2889fbc3fb3feafff98c3da9d60
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-29 18:26:12 -0300

    🎨project-card: default values

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -9,14 +9,15 @@ import { faLink, faTrash, faPencil, } from '@fortawesome/free-solid-svg-icons';
 })
 export class ProjectCardComponent implements OnInit {
 
+  @Input() project: Project;
+  show_description: boolean = false;
+  default_img: string = 'https://i.pinimg.com/564x/6b/02/cd/6b02cd228d391a474a582d8510bf4be8.jpg';
   icon = {
     link: faLink,
     pencil: faPencil,
     trash: faTrash
-  }
+  };
 
-  @Input() project: Project;
-  show_description: boolean = false;
 
   constructor() { }
 
