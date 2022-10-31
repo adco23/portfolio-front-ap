6bcc0754d64e3f3d5924171c290dd9e0f41f986b
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-31 01:01:55 -0300

    🚚 login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ import { SkillFilterPipe } from './pipes/skill-filter.pipe';
 import { XMarksComponent } from './components/icons/x-marks/x-marks.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 registerLocaleData(localeEs, 'es');
 
