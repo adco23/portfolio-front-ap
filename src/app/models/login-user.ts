bcb4debf4e0723f38af49453f691b4fa01aac23d
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-31 00:06:09 -0300

    ♻️login user model constructor add

--- src/app/models/login-user.ts
+++ src/app/models/login-user.ts
@@ -2,4 +2,10 @@ export class LoginUser {
   username?: String;
   email?: String;
   password: String;
+
+  constructor(password: String, username: String, email: String) {
+    this.password = password;
+    this.username = username;
+    this.email = email;
+  }
 }
