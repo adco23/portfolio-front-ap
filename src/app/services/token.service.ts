fc3ae8336c5969545aab292a0a4e07dc66034bb9
Adriana Coronel <adriana.coronel@plerk.io>
2022-10-31 00:40:16 -0300

    ✨token service add

---
+++ src/app/services/token.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUsername';
+const AUTHORITIES_KEY = 'AuthAuthorities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  roles: string[] = [];
+
+  private URL: string = `${environment.apiUrl}/api/v1`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public setToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return sessionStorage.getItem(TOKEN_KEY)!;
+  }
+
+  public setUsername(username: string): void {
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, username);
+  }
+
+  public getUsername(): string {
+    return sessionStorage.getItem(USERNAME_KEY)!;
+  }
+
+  public setAuthorities(authorities: string[]): void {
+    window.sessionStorage.removeItem(AUTHORITIES_KEY);
+    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): string[] {
+    this.roles = [];
+
+    let itemStorage = sessionStorage.getItem(AUTHORITIES_KEY);
+
+    return this.roles = itemStorage
+      ? [...JSON.parse(itemStorage)]
+      : [];
+  }
+
+  public logout(): void {
+    window.sessionStorage.clear();
+  }
+}
